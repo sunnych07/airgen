1a116d740b752753c5ca02a3db108a1c15b4412f
sunnych07 <sunnychundu5152@gmail.com>
2026-09-13 15:23:50 +0530

    open

---
+++ fix_modal_img.py
@@ -0,0 +1,13 @@
+﻿import re
+
+with open(r'C:\airgen\index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+bad_line = "document.getElementById('modalBg').style.backgroundImage = url('');"
+good_line = "document.getElementById('modalBg').style.backgroundImage = \"url('\" + data.img + \"')\";"
+
+content = content.replace(bad_line, good_line)
+
+with open(r'C:\airgen\index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Fixed background image JS bug.")
