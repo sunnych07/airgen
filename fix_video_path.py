3e89bb090ca08809d190544df3b7b66aab4aa44b
sunnych07 <sunnychundu5152@gmail.com>
2026-09-13 16:16:48 +0530

    vedio

---
+++ fix_video_path.py
@@ -0,0 +1,8 @@
+﻿with open(r'C:\airgen\index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+content = content.replace('src="intro.mp4"', 'src="/intro.mp4"')
+
+with open(r'C:\airgen\index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+print("Updated video src to absolute path.")
